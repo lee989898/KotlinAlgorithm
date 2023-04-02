df62f59fc11fd65e66f1aea4fa4cd0b55a366159
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-04-03 01:20:15 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2028 ms, Memory: 119568 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.kt
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.kt
@@ -2,14 +2,16 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.StringTokenizer
 
+data class Point(val x: Int, val y: Int)
+
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     var stringTokenizer = StringTokenizer(readln())
     val num = stringTokenizer.nextToken().toInt()
-    val pair = mutableListOf<Pair<Int, Int>>()
+    val points = mutableListOf<Point>()
     repeat(num) {
         stringTokenizer = StringTokenizer(readln(), " ")
-        pair.add(Pair(stringTokenizer.nextToken().toInt(),stringTokenizer.nextToken().toInt()))
+        points.add(Point(stringTokenizer.nextToken().toInt(), stringTokenizer.nextToken().toInt()))
     }
-    val sortPair = pair.sortedWith(compareBy({it.first}, {it.second}))
-    sortPair.forEach { println("${it.first} ${it.second}") }
+    val sortPair = points.sortedWith(compareBy({ it.x }, { it.y }))
+    sortPair.forEach { println("${it.x} ${it.y}") }
 }
