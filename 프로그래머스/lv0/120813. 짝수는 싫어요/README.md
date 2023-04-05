cb5c1c2e71cd5933f9fe38aaffab54183fe16b5b
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-04-05 23:53:27 +0900

    [level 0] Title: 짝수는 싫어요, Time: 7.54 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/README.md
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/README.md
@@ -1,10 +1,10 @@
 # [level 0] 짝수는 싫어요 - 120813 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120813?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120813) 
 
 ### 성능 요약
 
-메모리: 63.3 MB, 시간: 15.12 ms
+메모리: 61.8 MB, 시간: 7.54 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.kt
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.kt
@@ -1,12 +1,3 @@
 class Solution {
-    fun solution(n: Int): List<Int> {
-        val answer = mutableListOf<Int>()
-        var num = n
-        while (num != 0) {
-            if (num % 2 != 0)
-                answer.add(num)
-            num--
-        }
-        return answer.sorted()
-    }
+    fun solution(n: Int) = (0..n).filter { it % 2 != 0 }.toIntArray()
 }
