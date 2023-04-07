5390f757cd192cb6f7ce3e6b2aa70dd83afb6aeb
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-04-08 00:43:24 +0900

    [Bronze IV] Title: 숫자의 합, Time: 132 ms, Memory: 15900 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.kt
@@ -0,0 +1,18 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val num = readln().toInt()
+    val nums = readln().toList()
+
+    bw.write("${nums.sumOf { it.digitToInt() }}")
+    
+    bw.flush()
+    bw.close()
+    close()
+}
