a2ba5f270bd7b4e3d03e84f2fe52698a1abe4e11
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-04-08 12:35:06 +0900

    [level 0] Title: 숫자 찾기, Time: 14.22 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.kt
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.kt
@@ -1,12 +1,3 @@
 class Solution {
-    fun solution(num: Int, k: Int): Int {
-        var answer: Int = -1
-        num.toString().forEachIndexed { index, char ->
-            print(char)
-            if (char.toString() == k.toString() && answer == -1) {
-                answer = index + 1
-            }
-        }
-        return answer
-    }
+    fun solution(num: Int, k: Int) = "t$num".indexOf(k.toString())
 }
