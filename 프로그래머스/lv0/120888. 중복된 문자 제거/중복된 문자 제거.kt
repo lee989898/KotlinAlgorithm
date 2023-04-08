0897b7a027c401f003e210afa09151dd099224df
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-04-08 11:05:14 +0900

    [level 0] Title: 중복된 문자 제거, Time: 13.03 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.kt
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.kt
@@ -1,16 +1,3 @@
 class Solution {
-    fun solution(my_string: String): String {
-        var answer: String = ""
-        val set = mutableSetOf<Char>()
-        val list = mutableListOf<Char>()
-
-        my_string.forEach {
-            if (!set.contains(it)) {
-                set.add(it)
-                list.add(it)
-            }
-        }
-
-        return list.joinToString("")
-    }
+    fun solution(my_string: String) = my_string.toList().distinct().joinToString("")
 }
