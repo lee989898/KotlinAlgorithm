1ea9fbd03afccabdee2e4fa11d39cd4387a4cce7
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-04-08 17:14:30 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 13264 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.kt
+++ 백준/Silver/2563. 색종이/색종이.kt
@@ -7,18 +7,13 @@ import java.util.StringTokenizer
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val n = readln().toInt()
-
-    var stringTokenizer: StringTokenizer
     val array2D = Array(100) { Array(100) { 0 } }
-
-    var a: Int
-    var b: Int
     var result = 0
 
     repeat(n) {
-        stringTokenizer = StringTokenizer(readln(), " ")
-        a = stringTokenizer.nextToken().toInt()
-        b = stringTokenizer.nextToken().toInt()
+        val stringTokenizer = StringTokenizer(readln(), " ")
+        val a = stringTokenizer.nextToken().toInt()
+        val b = stringTokenizer.nextToken().toInt()
 
         for (i in a until a + 10) {
             for (j in b until b + 10) {
