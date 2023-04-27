6b9e35730829fe7a79a742aff301c9260ebdeaa2
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-04-27 12:06:06 +0900

    [level 0] Title: 짝수의 합, Time: 7.60 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.kt
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.kt
@@ -1,11 +1,3 @@
 class Solution {
-    fun solution(n: Int): Int {
-        var answer: Int = 0
-        for(index in 2..n) {
-            if(index % 2 == 0) {
-                answer += index
-            }
-        }
-        return answer
-    }
+    fun solution(n: Int) = (1..n).filter { it % 2 == 0 }.sum()
 }
