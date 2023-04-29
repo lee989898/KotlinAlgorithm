6853607035f89b19a85d337d40dc0ecac15e04c3
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-04-29 22:36:44 +0900

    [level 0] Title: 제곱수 판별하기, Time: 2.69 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.kt
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.kt
@@ -1,5 +1,7 @@
-import kotlin.math.sqrt
-
 class Solution {
-    fun solution(n: Int) = if (sqrt(n.toDouble()).toInt() * sqrt(n.toDouble()).toInt() == n) 1 else 2
+    fun solution(n: Int): Int {
+        var answer: Int = 2
+        (1..n).forEach { if (it * it == n) answer = 1 }
+        return answer
+    }
 }
