be041268c01ebf3d0b285965330dab33f0d76436
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-04-29 13:36:29 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.07 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.kt
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.kt
@@ -1,3 +1,3 @@
 class Solution {
-    fun solution(my_string: String, letter: String) = my_string.filter { it != letter[0] }
+    fun solution(my_string: String, letter: String) = my_string.filter { it.toString() != letter }
 }
