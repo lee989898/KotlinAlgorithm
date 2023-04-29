f63f59c7a61aad2962fabf8b88c19a0dee277bef
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-04-29 23:20:49 +0900

    [level 0] Title: 암호 해독, Time: 0.08 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.kt
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.kt
@@ -1,3 +1,3 @@
 class Solution {
-    fun solution(cipher: String, code: Int) = cipher.filterIndexed { index, c -> (index + 1) % code  == 0 }
+    fun solution(cipher: String, code: Int) = cipher.filterIndexed { index, _ -> (index + 1) % code == 0 }
 }
