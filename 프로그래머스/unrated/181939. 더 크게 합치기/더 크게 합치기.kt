d6699b6315245f7faedaf1865dde684f7aea89e1
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-04-29 22:54:28 +0900

    [unrated] Title: 더 크게 합치기, Time: 14.23 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.kt
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.kt
@@ -1,6 +1,5 @@
+import kotlin.math.max
+
 class Solution {
-    fun solution(a: Int, b: Int) = if ("$a$b".toInt() > "$b$a".toInt())
-        "$a$b".toInt()
-    else
-        "$b$a".toInt()
+    fun solution(a: Int, b: Int) = max("$a$b".toInt(), "$b$a".toInt())
 }
