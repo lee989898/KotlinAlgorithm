12b51c4d721b05e26429ce4ea7ba0f959dd2a6da
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-04-29 14:49:02 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 11.90 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.kt
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.kt
@@ -1,11 +1,3 @@
 class Solution {
-    fun solution(my_string: String, n: Int): String {
-        var answer: String = ""
-        my_string.forEach { char ->
-            repeat(n) {
-                answer += char
-            }
-        }
-        return answer
-    }
+    fun solution(my_string: String, n: Int) = my_string.map { it.toString().repeat(n) }.joinToString("")
 }
