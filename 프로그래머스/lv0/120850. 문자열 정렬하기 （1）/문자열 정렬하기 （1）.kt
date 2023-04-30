f72dbe0af73e26cee002eed478fc2865eda3c47e
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-05-01 01:11:25 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 16.09 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.kt
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.kt
@@ -1,3 +1,3 @@
 class Solution {
-    fun solution(my_string: String) = my_string.filter { it in '0'..'9' }.map { it.digitToInt() }.sorted()
+    fun solution(my_string: String) = my_string.filter { it.isDigit() }.map { it.digitToInt() }.sorted()
 }
