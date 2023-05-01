9ff2a1ccb3a5cd2d67046e32a1f35c6552091201
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-05-01 15:12:43 +0900

    [level 0] Title: 외계행성의 나이, Time: 11.50 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.kt
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.kt
@@ -1,6 +1,3 @@
 class Solution {
-    fun solution(age: Int): String {
-        val alphabet = ('a'..'z').toList()
-        return age.toString().map { alphabet[it - '0'] }.joinToString("")
-    }
+    fun solution(age: Int) = age.toString().map { (49 + it.code).toChar() }.joinToString("")
 }
