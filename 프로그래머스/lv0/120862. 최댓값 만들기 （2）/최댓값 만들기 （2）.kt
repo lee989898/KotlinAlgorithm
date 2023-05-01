b2fcd74b52d79bcaa4558ba74a9ffbb90d9a4ab2
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-05-01 13:14:14 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 17.27 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.kt
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.kt
@@ -1,15 +1,8 @@
+import kotlin.math.max
+
 class Solution {
     fun solution(numbers: IntArray): Int {
-        var answer: Int = numbers[0] * numbers[1]
-        for (i in 0 until numbers.size - 1) {
-            for (j in i + 1 until numbers.size) {
-                println("$i $j")
-                val num = numbers[i] * numbers[j]
-                if (num > answer) {
-                    answer = num
-                }
-            }
-        }
-        return answer
+        numbers.sort()
+        return max(numbers[0] * numbers[1], numbers[numbers.lastIndex - 1] * numbers[numbers.lastIndex])
     }
 }
