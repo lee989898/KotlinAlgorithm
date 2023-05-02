f2db6b8ce3bc8536c3082ab8d7f5b7f698eb071f
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-05-02 16:35:49 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 10.59 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
@@ -1,13 +1,5 @@
 class Solution {
     fun solution(phone_number: String): String {
-        var answer = ""
-
-        repeat(phone_number.length - 4) {
-            answer += "*"
-        }
-        
-        answer += phone_number.takeLast(4)
-
-        return answer
+        return "${"".padStart(phone_number.length - 4, '*')}${phone_number.takeLast(4)}"
     }
 }
