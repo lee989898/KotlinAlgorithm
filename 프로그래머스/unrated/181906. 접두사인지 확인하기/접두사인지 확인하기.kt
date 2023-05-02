4abee9a0b9ac1c97e623564011f496800e990a0a
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-05-02 19:24:00 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 6.15 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.kt
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.kt
@@ -1,11 +1,3 @@
 class Solution {
-    fun solution(my_string: String, is_prefix: String): Int {
-        var answer: Int = 0
-
-            if (my_string.take(is_prefix.length) == is_prefix)
-                answer = 1
-        
-
-        return answer
-    }
+    fun solution(my_string: String, is_prefix: String): Int = if (my_string.startsWith(is_prefix)) 1 else 0
 }
