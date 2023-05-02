fe5e286766a98eff7fb41bcf4f6f20471ddc4375
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-05-02 19:09:31 +0900

    [unrated] Title: 마지막 두 원소, Time: 13.41 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.kt
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.kt
@@ -1,13 +1,8 @@
 class Solution {
-    fun solution(num_list: IntArray): IntArray {
-        var answer: IntArray = num_list
-
-        answer += if(num_list[num_list.lastIndex] <= num_list[num_list.lastIndex - 1]) {
+    fun solution(num_list: IntArray) =
+        num_list + if (num_list[num_list.lastIndex] <= num_list[num_list.lastIndex - 1]) {
             num_list[num_list.lastIndex] * 2
         } else {
             num_list[num_list.lastIndex] - num_list[num_list.lastIndex - 1]
         }
-
-        return answer
-    }
 }
