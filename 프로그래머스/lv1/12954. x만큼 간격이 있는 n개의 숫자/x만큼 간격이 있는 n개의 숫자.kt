d2887564b691f264ee7e1a3aa21416fabc5b8fa8
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-05-03 14:22:40 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.03 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.kt
+++ 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.kt
@@ -1,7 +1,3 @@
-import kotlin.math.abs
-
 class Solution {
-    fun solution(x: Int, n: Int) = if (x < 0) (x downTo (x * n.toLong()) step abs(x.toLong())).toList()
-    else if(x > 0) (x..(x * n.toLong()) step x.toLong()).toList()
-    else List(n) { 0 }
+    fun solution(x: Int, n: Int) = LongArray(n) { x.toLong() * (it + 1) }
 }
