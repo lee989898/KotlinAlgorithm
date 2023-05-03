4f628eafe673381c42fc3aa3b28e80dbcfc3f157
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-05-03 12:02:33 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 13.30 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.kt
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.kt
@@ -1,9 +1,8 @@
+import java.util.*
+
 class Solution {
-    fun solution(my_string: String, num1: Int, num2: Int): String {
-        var answer: String = ""
-
-        my_string.forEachIndexed { index, c -> if (index == num1) answer += my_string[num2] else if (index == num2) answer += my_string[num1] else answer += my_string[index] }
-
-        return answer
+    fun solution(my_string: String, num1: Int, num2: Int) = my_string.toList().let {
+        Collections.swap(it, num1, num2)
+        it.joinToString("")
     }
 }
