dc4aa06cc943132a6eaf3aeb09025449503b277b
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-05-04 16:03:32 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.02 ms, Memory: 59 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(arr: Array<IntArray>): Int {
+        var answer: Int = 1
+
+        for (i in arr.indices) {
+            for (j in arr[i].indices) {
+                if (arr[i][j] != arr[j][i])
+                    answer = 0
+            }
+        }
+
+        return answer
+    }
+}
