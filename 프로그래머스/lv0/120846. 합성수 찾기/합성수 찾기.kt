36652245020da5b1375f5a42768902ee34ae4be1
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-05-04 16:43:04 +0900

    [level 0] Title: 합성수 찾기, Time: 2.41 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.kt
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.kt
@@ -1,9 +1,3 @@
 class Solution {
-    fun solution(n: Int): Int {
-        var answer: Int = 0
-
-        (1..n).forEach { a -> if ((1..a).count { b -> a % b == 0 } >= 3) answer += 1 }
-        
-        return answer
-    }
+    fun solution(n: Int) = (1..n).count { i -> (1..i).count { i % it == 0 } > 2 }
 }
