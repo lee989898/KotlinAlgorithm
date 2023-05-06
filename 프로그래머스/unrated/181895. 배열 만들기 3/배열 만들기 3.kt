451cc175a47d88258ea0a0cb903702ff7ae93fa9
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-05-06 18:27:04 +0900

    [unrated] Title: 배열 만들기 3, Time: 22.55 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.kt
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.kt
@@ -1,10 +1,3 @@
 class Solution {
-    fun solution(arr: IntArray, intervals: Array<IntArray>): IntArray {
-        var answer: IntArray = intArrayOf()
-        
-        answer += arr.slice(intervals[0][0]..intervals[0][1])
-        answer += arr.slice(intervals[1][0]..intervals[1][1])
-        
-        return answer
-    }
+    fun solution(arr: IntArray, intervals: Array<IntArray>) = intervals.flatMap { (left, right) -> arr.slice(left..right) }
 }
