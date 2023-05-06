c11b10af01f15014c5ca433662c2938ff7777495
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-05-06 22:50:50 +0900

    [unrated] Title: 주사위 게임 2, Time: 16.49 ms, Memory: 63.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(a: Int, b: Int, c: Int): Int {
+        val set = setOf(a, b, c)
+        return when (set.size) {
+            3 -> a + b + c
+            2 -> (a + b + c) * (a * a + b * b + c * c)
+            1 -> (a + b + c) * (a * a + b * b + c * c) * (a * a * a + b * b * b + c * c * c)
+            else -> -1
+        }
+    }
+}
