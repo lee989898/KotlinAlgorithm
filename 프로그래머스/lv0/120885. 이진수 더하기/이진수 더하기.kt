07c3e52d353e70d74f12b4100407c40a0c8c7460
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-05-08 12:30:17 +0900

    [level 0] Title: 이진수 더하기, Time: 0.79 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.kt
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.kt
@@ -1,3 +1,3 @@
 class Solution {
-    fun solution(bin1: String, bin2: String) = Integer.toBinaryString(Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2))
+    fun solution(bin1: String, bin2: String) = Integer.toBinaryString(bin1.toInt(2) + bin2.toInt(2))
 }
