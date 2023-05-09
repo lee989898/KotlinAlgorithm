4f16ebf5c62ad2b2c3bdd67b57ce45870081e732
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-05-09 10:04:56 +0900

    [unrated] Title: 순서 바꾸기, Time: 13.69 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.kt
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.kt
@@ -1,10 +1,3 @@
 class Solution {
-    fun solution(num_list: IntArray, n: Int): IntArray {
-        var answer: IntArray = intArrayOf()
-
-        answer += num_list.slice(n until num_list.size)
-        answer += num_list.slice(0 until n)
-        
-        return answer
-    }
+    fun solution(num_list: IntArray, n: Int) = (num_list + num_list).copyOfRange(n, n + num_list.size)
 }
