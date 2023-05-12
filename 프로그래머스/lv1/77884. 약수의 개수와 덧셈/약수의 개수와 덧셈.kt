0850d0a88afdf86947cf32c560d04e5bb4ffaa9a
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-05-12 15:26:44 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 4.69 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
@@ -1,16 +1,4 @@
 class Solution {
-    fun solution(left: Int, right: Int): Int {
-        var result = 0
-
-        (left..right).forEach { num ->
-            var answer = 0
-            (1..num).forEach { if (num % it == 0) answer++ }
-            if (answer % 2 == 0)
-                result += num
-            else
-                result -= num
-        }
-
-        return result
-    }
+    fun solution(left: Int, right: Int) =
+        (left..right).sumOf { i -> if ((1..i).filter { i % it == 0 }.size % 2 == 0) i else -i }
 }
