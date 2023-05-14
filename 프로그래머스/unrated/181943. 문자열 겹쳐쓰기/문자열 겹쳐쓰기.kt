2a412c8402f730b1a9799ec5e6fc089a32f8c05e
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-05-14 20:22:11 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 26.68 ms, Memory: 61.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.kt
@@ -0,0 +1,4 @@
+class Solution {
+    fun solution(my_string: String, overwrite_string: String, s: Int) =
+        my_string.slice(0 until s) + overwrite_string + my_string.slice(overwrite_string.length + s until my_string.length)
+}
