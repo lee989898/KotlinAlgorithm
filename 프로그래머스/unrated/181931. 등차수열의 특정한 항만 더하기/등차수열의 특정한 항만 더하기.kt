3dc71cca856123ff5157e5037d2b871985e23561
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-05-22 00:08:20 +0900

    [unrated] Title: 등차수열의 특정한 항만 더하기, Time: 0.02 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.kt
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.kt
@@ -1,15 +1,13 @@
 class Solution {
     fun solution(a: Int, d: Int, included: BooleanArray): Int {
         var answer: Int = 0
-        var _a = a
-        
+
         repeat(included.size) {
             if (included[it]) {
-                answer += _a
+                answer += a + d * it
             }
-            _a += d
         }
-        
+
         return answer
     }
 }
