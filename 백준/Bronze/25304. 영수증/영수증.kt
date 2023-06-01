465108ab12fdc6b0ea9753a274a295205c13bf98
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-01 11:10:04 +0900

    [Bronze V] Title: 영수증, Time: 136 ms, Memory: 20704 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.kt
+++ 백준/Bronze/25304. 영수증/영수증.kt
@@ -2,28 +2,26 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.util.StringTokenizer
 
-fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
-    var stringTokenizer: StringTokenizer
-    var total = StringTokenizer(readln()).nextToken().toInt()
-    val num = StringTokenizer(readln()).nextToken().toInt()
-
-    repeat(num) {
-        stringTokenizer = StringTokenizer(readln(), " ")
-        val bill = stringTokenizer.nextToken().toInt()
-        val count = stringTokenizer.nextToken().toInt()
-        total -= bill * count
+    
+    val total = br.readLine().toInt()
+    val case = br.readLine().toInt()
+    var sum = 0
+    
+    repeat(case) {
+        val (a, b) = br.readLine().split(" ").map { it.toInt() }
+        sum += a * b
     }
-
-    if (total == 0) {
+    
+    if (total == sum)
         bw.write("Yes")
-    } else {
+    else
         bw.write("No")
-    }
-
+    
     bw.flush()
     bw.close()
-    close()
+    br.close()
 }
