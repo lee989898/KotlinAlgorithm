837066633a23596762bba334e5bcd1edc1c21ee8
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-02 10:17:23 +0900

    [Bronze III] Title: 최소, 최대, Time: 628 ms, Memory: 158832 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.kt
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.kt
@@ -2,23 +2,16 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.util.StringTokenizer
 
-fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    val N = StringTokenizer(readln()).nextToken().toInt()
-    val nums = mutableListOf<Int>()
-
-    val num = StringTokenizer(readln(), " ")
-    while (num.hasMoreTokens()) {
-        nums.add(num.nextToken().toInt())
-    }
-
-    bw.write("${nums.min()} ${nums.max()}")
-
+    val num = br.readLine().toInt()
+    val list = br.readLine().split(" ").map { it.toInt() }
+    bw.write("${list.min()} ${list.max()}")
 
     bw.flush()
     bw.close()
-    close()
+    br.close()
 }
