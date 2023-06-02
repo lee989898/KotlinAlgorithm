efbd6e3db1bda7e520b18531189be51a5ec0cc6b
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-02 09:15:41 +0900

    [Bronze V] Title: 개수 세기, Time: 140 ms, Memory: 21248 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.kt
+++ 백준/Bronze/10807. 개수 세기/개수 세기.kt
@@ -2,25 +2,17 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.util.StringTokenizer
 
-fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
-    val N = StringTokenizer(readln()).nextToken().toInt()
 
-    val integers = mutableListOf<Int>()
-
-    val integer = StringTokenizer(readln(), " ")
-
-    while (integer.hasMoreTokens()) {
-        integers.add(integer.nextToken().toInt())
-    }
-
-    val findNum = StringTokenizer(readln()).nextToken().toInt()
-
-    bw.write("${integers.count { it == findNum }}")
+    val num = br.readLine().toInt()
+    val list = br.readLine().split(" ").map { it.toInt() }
+    val target = br.readLine().toInt()
+    bw.write("${list.count { it == target }}")
 
     bw.flush()
     bw.close()
-    close()
+    br.close()
 }
