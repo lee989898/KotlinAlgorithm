4d5f6a9b826dbcc551ba292639eab3c936bdc14c
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-02 09:10:27 +0900

    [Bronze V] Title: A+B - 4, Time: 132 ms, Memory: 20676 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.kt
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.kt
@@ -2,21 +2,21 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.util.StringTokenizer
 
-fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    var s: String?
-
-    while (readLine().also { s = it } != null) {
-        val stringTokenizer = StringTokenizer(s)
-        val num1 = stringTokenizer.nextToken().toInt()
-        val num2 = stringTokenizer.nextToken().toInt()
-        bw.write((num1 + num2).toString() + "\n")
+    while (true) {
+        try {
+            val (a, b) = br.readLine().split(" ").map { it.toInt() }
+            bw.write("${a + b}\n")
+        } catch (e: Exception) {
+            break
+        }
     }
- 
+
     bw.flush()
     bw.close()
-    close()
+    br.close()
 }
