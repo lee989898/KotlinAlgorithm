013c73726ae2c9b89417d213ac6184160b0449b2
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-03 09:33:18 +0900

    [Bronze II] Title: 상수, Time: 140 ms, Memory: 20648 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.kt
+++ 백준/Bronze/2908. 상수/상수.kt
@@ -2,21 +2,20 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.util.StringTokenizer
 
-fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    val stringTokenizer = StringTokenizer(readln(), " ")
-    val num1 = stringTokenizer.nextToken().reversed()
-    val num2 = stringTokenizer.nextToken().reversed()
+    val (a, b) = br.readLine().split(" ").map { it.toList().reversed().joinToString("").toInt() }
 
-    if (num1 > num2)
-        bw.write(num1)
-    else
-        bw.write(num2)
+    if (a > b) {
+        bw.write("$a")
+    } else {
+        bw.write("$b")
+    }
 
     bw.flush()
     bw.close()
-    close()
+    br.close()
 }
