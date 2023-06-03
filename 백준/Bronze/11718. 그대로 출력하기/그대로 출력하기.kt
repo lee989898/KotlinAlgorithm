088a4c74707ebed126df5af5f804e8fb97a9215c
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-03 10:08:25 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 84 ms, Memory: 12132 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.kt
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.kt
@@ -1,9 +1,19 @@
-import java.util.Scanner
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
 fun main() {
-    val sc = Scanner(System.`in`)
-    while (sc.hasNextLine()) {
-        val input = sc.nextLine()
-        println(input)
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    while(true) {
+        val str = br.readLine() ?: break
+
+        bw.write("$str\n")
     }
+
+    bw.flush()
+    bw.close()
+    br.close()
 }
