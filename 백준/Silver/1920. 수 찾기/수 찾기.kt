468889f9c46b924ab69ea001e1aa6cda54a571c3
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-04 16:31:55 +0900

    [Silver IV] Title: 수 찾기, Time: 832 ms, Memory: 63620 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.kt
+++ 백준/Silver/1920. 수 찾기/수 찾기.kt
@@ -1,17 +1,26 @@
-import java.io.*
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+var n: Int = 0
+var nList = listOf<Int>()
+var m: Int = 0
+var mList = listOf<Int>()
+var sortedNList = listOf<Int>()
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    val num = br.readLine().toInt()
-    val arr = br.readLine().split(" ").map { it.toInt() }.toIntArray().sorted()
+    n = br.readLine().toInt()
+    nList = br.readLine().split(" ").map { it.toInt() }
+    m = br.readLine().toInt()
+    mList = br.readLine().split(" ").map { it.toInt() }
+    sortedNList = nList.sorted()
 
-    val num2 = br.readLine().toInt()
-    val arr2 = br.readLine().split(" ").map { it.toInt() }
-
-    for (i in arr2) {
-        if (arr.binarySearch(i) >= 0) {
+    for (item in mList) {
+        if (binarySearch(item, 0, n - 1)) {
             bw.write("1\n")
         } else {
             bw.write("0\n")
@@ -21,4 +30,19 @@ fun main() {
     bw.flush()
     bw.close()
     br.close()
+}
+
+fun binarySearch(value: Int, start: Int, end: Int): Boolean {
+    if (start > end) {
+        return false
+    }
+
+    val medium = (start + end) / 2
+    return if (value < sortedNList[medium]) {
+        binarySearch(value, start, medium - 1)
+    } else if (value > sortedNList[medium]) {
+        binarySearch(value, medium + 1, end)
+    } else {
+        true
+    }
 }
