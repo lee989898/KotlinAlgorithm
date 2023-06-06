227d352e99be092ffd4f69b794cc646b82512eec
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-06 09:26:37 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,11 @@
+ascending = [1, 2, 3, 4, 5, 6, 7, 8]
+descending = [8, 7, 6, 5, 4, 3, 2, 1]
+
+scale = list(map(int, input().split()))
+
+if scale == ascending:
+    print('ascending')
+elif scale == descending:
+    print('descending')
+else:
+    print('mixed')
