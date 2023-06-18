2aa5b17d0ff9a3194cc6387fcaed4f51486115de
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-18 22:22:36 +0900

    [level 2] Title: 올바른 괄호, Time: 7.86 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,6 +1,7 @@
 def solution(s):
-    pair = 0
-    for x in s:
-        if pair < 0: break
-        pair = pair + 1 if x == "(" else pair - 1 if x == ")" else pair
-    return pair == 0
+    wt = 0
+    for c in s :
+        if c == '(' : wt += 1
+        elif c == ')' : wt -= 1
+        if wt < 0 : return False
+    return wt == 0
