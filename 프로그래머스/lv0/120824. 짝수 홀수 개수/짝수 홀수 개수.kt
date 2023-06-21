0073814ddd2b9ce3f3ecf21ce4d3f1a9977750dc
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-21 23:03:56 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.kt
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.kt
@@ -1,6 +1,7 @@
 class Solution {
     fun solution(num_list: IntArray): IntArray {
-        val evenCount = num_list.count { it % 2 == 0}
-        return intArrayOf(evenCount, num_list.size - evenCount)
+        val even = num_list.count { it % 2 == 0 }
+        val odd = num_list.size - even
+        return intArrayOf(even, odd)
     }
 }
