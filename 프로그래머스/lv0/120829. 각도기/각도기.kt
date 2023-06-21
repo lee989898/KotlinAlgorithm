0c33b6bd807495a17c293ecc7398c70ea22eaa79
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-21 22:33:43 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 59.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.kt
+++ 프로그래머스/lv0/120829. 각도기/각도기.kt
@@ -1,8 +1,8 @@
 class Solution {
     fun solution(angle: Int) = when {
-        angle in 1 until 90 -> 1
+        angle in 1..89 -> 1
         angle == 90 -> 2
-        angle in 91 until 180 -> 3
+        angle in 91..179 -> 3
         else -> 4
     }
 }
