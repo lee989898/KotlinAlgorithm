4f6b5d0deec836d93bb80ee42eb92fb27dd5c4bf
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-21 23:46:19 +0900

    [unrated] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.kt
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.kt
@@ -1,12 +1,3 @@
 class Solution {
-    fun solution(numbers: IntArray, n: Int): Int {
-        var answer: Int = 0
-        var i = 0
-        
-        while(answer <= n) {
-            answer += numbers[i++]
-        }
-        
-        return answer
-    }
+    fun solution(numbers: IntArray, n: Int) = numbers.fold(0) { total, num -> if (n >= total) total + num else total }
 }
