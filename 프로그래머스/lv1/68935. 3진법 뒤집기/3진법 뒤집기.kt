3b92857103ba901266118388fb7f140703c61994
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-22 17:29:08 +0900

    [level 1] Title: 3진법 뒤집기, Time: 7.97 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.kt
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.kt
@@ -1,22 +1,5 @@
-import kotlin.math.*
-
 class Solution {
     fun solution(n: Int): Int {
-        var n = n
-        var answer: Int = 0
-        var str = ""
-        
-        while(n != 0) {
-            str += (n % 3).toString()
-            n /= 3
-        }
-        
-        var i = str.length - 1
-        for(char in str) {
-            answer += char.digitToInt() * ((3.0).pow(i)).toInt()
-            i--
-        }
-        
-        return answer
+        return n.toString(3).reversed().toInt(3)
     }
 }
