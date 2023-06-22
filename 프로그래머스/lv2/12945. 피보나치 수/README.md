537be76d02798954c29dd65af251d0fae5bad2cd
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-22 20:56:54 +0900

    [level 2] Title: 피보나치 수, Time: 6.13 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/README.md
+++ 프로그래머스/lv2/12945. 피보나치 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피보나치 수 - 12945 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945) 
 
 ### 성능 요약
 
-메모리: 60.9 MB, 시간: 24.19 ms
+메모리: 61 MB, 시간: 6.13 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.kt
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.kt
@@ -1,14 +1,11 @@
-val list = MutableList<Int>(100001){ 0 }
-
 class Solution {
     fun solution(n: Int): Int {
-        list[0] = 0
-        list[1] = 1
-        
+        var ans = Array(n + 1) { 0 }
+        ans[1] = 1
         for(i in 2..n) {
-            list[i] = (list[i - 1] % 1234567 + list[i - 2] % 1234567) % 1234567
+            ans[i] = (ans[i - 1] + ans[i - 2]) % 1234567
         }
                 
-        return list[n] % 1234567
+        return ans[n] 
     }
 }
