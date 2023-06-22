fe687a12cab118ba885a2afe0f92cd1b4615f5c5
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-22 14:48:57 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 9.32 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.kt
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.kt
@@ -1,20 +1,3 @@
 class Solution {
-    fun solution(s: String): Boolean {
-        var answer = true
-        
-        if (s.length == 4 || s.length == 6) {
-            answer = true
-        } else {
-            return false
-        }
-        
-        for(char in s) {            
-            if(char in 'a'..'z' || char in 'A'..'Z') {
-                answer = false
-                break
-            }
-        }
-                
-        return answer
-    }
+    fun solution(s: String) = (s.length == 4 || s.length == 6) && s.toIntOrNull() != null
 }
