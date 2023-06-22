b8ac1d77029de0abf738913b037402bd0a238d52
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-22 10:28:48 +0900

    [level 1] Title: 하샤드 수, Time: 6.41 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.kt
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.kt
@@ -1,4 +1,11 @@
 class Solution {
-    fun solution(x: Int) =
-        x % x.toString().sumOf { it.digitToInt() } == 0
+    fun solution(x: Int): Boolean {
+        val sum = x.toString().map { it.digitToInt() }.sum()
+        
+        return if (x % sum == 0) {
+            true
+        } else {
+            false
+        }
+    }
 }
