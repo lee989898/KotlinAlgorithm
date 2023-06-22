5b5da55d433959653e00e3614c30d51f4f2c46bd
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-22 10:01:27 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.kt
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.kt
@@ -1,11 +1,16 @@
+import kotlin.math.*
+
 class Solution {
     fun solution(n: Long): Long {
-        for (i in 1..n) {
-            if (i * i == n) {
-                return (i + 1L) * (i + 1L)
-            }
+        var answer: Long = 0
+        val sqrt = sqrt(n.toDouble()).toLong()
+        
+        if ((sqrt * sqrt) == n) {
+            answer = (sqrt + 1L) * (sqrt.toLong() + 1L)
+        } else {
+            answer = -1
         }
         
-        return -1
+        return answer
     }
 }
