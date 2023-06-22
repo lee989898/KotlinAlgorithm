19899c731913b865106522db96683ac13f610b40
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-22 11:28:52 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 27.97 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.kt
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.kt
@@ -1,5 +1,5 @@
+import kotlin.math.*
+
 class Solution {
-    fun solution(arr: IntArray) = arr.filter { it != arr.minOrNull() }.run {
-        this.ifEmpty { intArrayOf(-1) }
-    }
+    fun solution(arr: IntArray) = if(arr.size == 1) arrayOf(-1).toIntArray() else arr.filterNot { it == arr.minOrNull() }.toIntArray()
 }
