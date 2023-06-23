110e02527056b519e230c2fb16daa4a025d0abbe
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-06-23 18:04:13 +0900

    [level 1] Title: 모의고사, Time: 31.62 ms, Memory: 65.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.kt
@@ -0,0 +1,40 @@
+import kotlin.math.*
+
+class Solution {
+    fun solution(answers: IntArray): IntArray {
+        val answer = mutableListOf<Int>()
+        val one = listOf(1, 2, 3, 4, 5) // 5
+        val two = listOf(2, 1, 2, 3, 2, 4, 2, 5) // 8
+        val three = listOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5) // 10
+
+        var (a, b, c) = listOf(0, 0, 0)
+
+        for ((i, ans) in answers.withIndex()) {
+            if (ans == one[i % 5]) {
+                a++
+            }
+            if (ans == two[i % 8]) {
+                b++
+            }
+            if (ans == three[i % 10]) {
+                c++
+            }
+        }
+        val max = listOf(a, b, c).maxOrNull()
+
+        print("$a $b $c $max")
+
+        if (a == max) {
+            answer.add(1)
+        }
+        if (b == max) {
+            answer.add(2)
+        }
+
+        if (c == max) {
+            answer.add(3)
+        }
+
+        return answer.toIntArray()
+    }
+}
