b14affe818e12b131cdfe5db0f1588d13a996480
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-02 18:12:03 +0900

    [level 0] Title: 모음 제거, Time: 7.44 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.kt
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.kt
@@ -1,8 +1,3 @@
 class Solution {
-    fun solution(my_string: String) = my_string
-        .replace("a", "")
-        .replace("e", "")
-        .replace("i", "")
-        .replace("o", "")
-        .replace("u", "")
+    fun solution(my_string: String) = my_string.filterNot { "aeiou".contains(it) }
 }
