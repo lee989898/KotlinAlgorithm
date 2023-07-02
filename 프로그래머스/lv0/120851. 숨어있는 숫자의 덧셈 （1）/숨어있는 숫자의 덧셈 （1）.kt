bb37460d896da20dcf9afab5b4dada89954459f9
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-02 20:34:03 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 5.97 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.kt
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.kt
@@ -1,3 +1,3 @@
 class Solution {
-    fun solution(my_string: String) = my_string.filter { it in '1'..'9'}.map { it.digitToInt() }.sum()
+    fun solution(myString: String) = myString.filter { it.isDigit() }.map { c -> c.toString().toInt() }.sum()
 }
