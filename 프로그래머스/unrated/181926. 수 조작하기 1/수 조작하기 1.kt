5c2caa4f2e94e43b483fbcef20db3570cd472059
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-02 21:52:46 +0900

    [unrated] Title: 수 조작하기 1, Time: 1.71 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.kt
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.kt
@@ -1,4 +1,13 @@
 class Solution {
-    fun solution(n: Int, control: String) =
-        control.fold(n) { acc, c -> acc + if (c == 'w') 1 else if (c == 's') -1 else if (c == 'd') 10 else -10 }
+    fun solution(n: Int, control: String): Int {
+        var answer: Int = n
+        control.forEach { when(it) {
+            'w' -> answer += 1
+            's' -> answer -= 1
+            'd' -> answer += 10
+            'a' -> answer -= 10
+        }}
+        
+        return answer
+    }
 }
