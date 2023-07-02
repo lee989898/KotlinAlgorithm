62d8f28923955f3b4c6d71e5007e8df52c513f7c
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-02 20:46:25 +0900

    [unrated] Title: 문자열 반복해서 출력하기, Time: 118.35 ms, Memory: 46.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.kt
+++ 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.kt
@@ -1,6 +1,3 @@
 fun main(args: Array<String>) {
-		val input = readLine()!!.split(' ')
-    val s1 = input[0]
-    val a = input[1]!!.toInt()
-    print(s1.repeat(a))
+    readln().split(' ').let { print(it[0].repeat(it[1].toInt())) }
 }
