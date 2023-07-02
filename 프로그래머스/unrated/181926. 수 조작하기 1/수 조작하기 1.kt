30727501efd500ab9ba0b28311f780f45d1a8fc2
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-02 22:09:16 +0900

    [unrated] Title: 수 조작하기 1, Time: 6.61 ms, Memory: 59.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.kt
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.kt
@@ -1,13 +1,6 @@
 class Solution {
     fun solution(n: Int, control: String): Int {
-        var answer: Int = n
-        control.forEach { when(it) {
-            'w' -> answer += 1
-            's' -> answer -= 1
-            'd' -> answer += 10
-            'a' -> answer -= 10
-        }}
-        
-        return answer
+        val map = mapOf('w' to 1, 's' to -1, 'd' to 10, 'a' to -10)
+        return control.fold(n) { acc, i -> acc + map[i]!! }
     }
 }
