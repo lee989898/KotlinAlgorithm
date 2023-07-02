6cda4cc1f931a27c2b8af25933955cdcfc53e681
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-02 17:01:35 +0900

    [level 0] Title: 모음 제거, Time: 7.82 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.kt
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.kt
@@ -1,10 +1,8 @@
 class Solution {
-    fun solution(my_string: String): String {
-        var answer: String = my_string
-        val words = listOf("a", "e", "i", "o", "u")
-        for(word in words) {
-           answer = answer.replace(word, "")
-        }
-        return answer
-    }
+    fun solution(my_string: String) = my_string
+        .replace("a", "")
+        .replace("e", "")
+        .replace("i", "")
+        .replace("o", "")
+        .replace("u", "")
 }
