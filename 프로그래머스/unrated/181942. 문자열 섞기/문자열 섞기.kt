99fc09adc0f3daf082aaab173b348a92a009fb6f
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-03 14:09:56 +0900

    [unrated] Title: 문자열 섞기, Time: 17.02 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.kt
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.kt
@@ -1,4 +1,10 @@
 class Solution {
-
-    fun solution(str1: String, str2: String) = str1.zip(str2).joinToString("") { (a, b) -> "$a$b" }
+    fun solution(str1: String, str2: String): String {
+        var answer = ""
+        for (i in str1.indices) {
+            answer += str1[i]
+            answer += str2[i]
+        }
+        return answer
+    }
 }
