fff088d47277e30f2f8a4f8a033a5627267df24a
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-04 10:21:13 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 19.99 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.kt
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.kt
@@ -1,5 +1,3 @@
-import kotlin.math.pow
-
 class Solution {
-    fun solution(num_list: IntArray) = if(num_list.reduce { acc, i -> acc * i } < num_list.sum().toDouble().pow(2)) 1 else 0
+    fun solution(num_list: IntArray) = if(num_list.fold(1) { acc, i -> acc * i } < num_list.sum() * num_list.sum()) 1 else 0
 }
