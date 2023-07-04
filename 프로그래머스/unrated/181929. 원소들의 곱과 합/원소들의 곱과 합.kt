dbaa4b7d2f9b0fa2f92e0f043176ce3f1b045f41
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-04 10:23:40 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 16.39 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.kt
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.kt
@@ -1,3 +1,5 @@
 class Solution {
-    fun solution(num_list: IntArray) = if(num_list.fold(1) { acc, i -> acc * i } < num_list.sum() * num_list.sum()) 1 else 0
+    fun solution(num_list: IntArray) = if(num_list.fold(1) { acc, i -> acc * i } < num_list.sum().pow()) 1 else 0
 }
+
+fun Int.pow() = this * this
