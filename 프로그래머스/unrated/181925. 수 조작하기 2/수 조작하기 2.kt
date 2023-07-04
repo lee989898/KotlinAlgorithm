5c357eaae69f5b0c61faa938483b87545f5e5709
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-04 11:58:27 +0900

    [unrated] Title: 수 조작하기 2, Time: 47.04 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.kt
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.kt
@@ -1,18 +1,12 @@
 class Solution {
     fun solution(numLog: IntArray): String {
-        val map = mapOf(1 to "w", -1 to "s", 10 to "d", -10 to "a")
-        val nums = mutableListOf<Int>()
-
-        for (i in 1 until numLog.size) {
-            nums.add(numLog[i] - numLog[i - 1])
-        }
-
-        var answer: String = ""
-
-        nums.forEach {
-            answer += map[it]
-        }
-
-        return answer
+        return (1..numLog.lastIndex).map {
+            when (numLog[it] - numLog[it - 1]) {
+                1 -> 'w'
+                -1 -> 's'
+                10 -> 'd'
+                else -> 'a'
+            }
+        }.joinToString("")
     }
 }
