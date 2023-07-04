cd0483db4075eba64b33e3eca28c631f48c63a1d
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-04 10:32:38 +0900

    [unrated] Title: 등차수열의 특정한 항만 더하기, Time: 20.16 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.kt
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.kt
@@ -1,3 +1,3 @@
 class Solution {
-    fun solution(a: Int, d: Int, included: BooleanArray) = (a until a + d * included.size step d).toList().zip(included.toList()).filter { it.second }.sumOf { it.first }
+    fun solution(a: Int, d: Int, included: BooleanArray) = included.indices.filter { included[it] }.sumOf { a + d * it }
 }
