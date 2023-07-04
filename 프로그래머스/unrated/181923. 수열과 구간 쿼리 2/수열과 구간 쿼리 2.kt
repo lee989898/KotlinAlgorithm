4e42ad5f0f5a222e3611df6db1551462b7d7b2b4
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-04 11:43:47 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 40.84 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.kt
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.kt
@@ -1,13 +1,5 @@
 class Solution {
-    fun solution(arr: IntArray, queries: Array<IntArray>): IntArray {
-        val answer = mutableListOf<Int>()
-        
-        for (querie in queries) {
-            val temp = arr.slice(querie[0]..querie[1]).sorted()
-            
-            answer.add(temp.find { it > querie[2] } ?: -1)
-        }
-
-        return answer.toIntArray()
+    fun solution(arr: IntArray, queries: Array<IntArray>) = queries.map { (s, e, k) ->
+        arr.sliceArray(s..e).sorted().find { it > k } ?: - 1
     }
 }
