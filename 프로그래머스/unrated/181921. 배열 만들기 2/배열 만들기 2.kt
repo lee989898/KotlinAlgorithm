3c22625c533872ab11cbce4e81feac231aa7f236
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-05 21:31:53 +0900

    [unrated] Title: 배열 만들기 2, Time: 20.75 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.kt
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.kt
@@ -1,23 +1,5 @@
 class Solution {
-    fun solution(l: Int, r: Int): IntArray {
-        var answer = mutableListOf<Int>()
-        
-        for(i in l..r) {
-            val c = i.toString()
-            
-            if(c.contains("1") || c.contains("2") || c.contains("3") || c.contains("4") || c.contains("6") || c.contains("7") || c.contains("8") ||
-                    c.contains("9"))
-                continue
-            
-            if(c.contains("5") || c.contains("0"))
-                answer.add(i)
-            
-        }
-        
-        if(answer.isEmpty()) {
-            return intArrayOf(-1)
-        } else {
-            return answer.toIntArray()
-        }
+    fun solution(l: Int, r: Int): List<Int> {
+        return (l..r).filter { it.toString().all { ch -> ch == '0' || ch == '5' } }.takeIf { it.isNotEmpty() } ?: listOf(-1)
     }
 }
