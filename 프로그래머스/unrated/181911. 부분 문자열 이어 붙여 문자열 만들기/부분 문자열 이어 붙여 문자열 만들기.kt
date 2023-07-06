4e67a9f710698731410b4b772dfd24ee85402e44
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-06 09:49:18 +0900

    [unrated] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 13.29 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.kt
+++ 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.kt
@@ -1,4 +1,11 @@
 class Solution {
-    fun solution(my_strings: Array<String>, parts: Array<IntArray>) =
-        my_strings.mapIndexed { i, v -> my_strings[i].slice(parts[i][0]..parts[i][1]) }.joinToString("")
+    fun solution(my_strings: Array<String>, parts: Array<IntArray>): String {
+        var answer: String = ""
+        
+        for(i in my_strings.indices) {
+            answer += my_strings[i].slice(parts[i][0]..parts[i][1])
+        }
+        
+        return answer
+    }
 }
