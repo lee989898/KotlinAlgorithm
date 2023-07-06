60a77d4edb8658118f53826b452be012a4a6e31c
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-06 10:10:43 +0900

    [unrated] Title: 접미사 배열, Time: 16.67 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.kt
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.kt
@@ -1,11 +1,3 @@
 class Solution {
-    fun solution(my_string: String): Array<String> {
-        var answer: Array<String> = arrayOf<String>()
-
-        my_string.indices.forEach { answer += my_string.slice(it until my_string.length) }
-
-        answer.sort()
-
-        return answer
-    }
+    fun solution(myString: String) = myString.indices.map(myString::substring).sorted()
 }
