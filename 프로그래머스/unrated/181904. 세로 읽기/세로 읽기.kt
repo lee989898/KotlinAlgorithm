c9f142306cf5b352f13d35ce5357a69b7c90586f
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-07 11:44:37 +0900

    [unrated] Title: 세로 읽기, Time: 20.19 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.kt
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.kt
@@ -1,3 +1,5 @@
 class Solution {
-    fun solution(my_string: String, m: Int, c: Int) = my_string.chunked(m).map { it[c - 1] }.joinToString("")
+    fun solution(my_string: String, m: Int, c: Int): String {
+        return my_string.chunked(m).map { it[c - 1] }.joinToString("")
+    }
 }
