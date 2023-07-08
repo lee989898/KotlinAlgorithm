905eb5a51f6b69552045cf64d5bd505d6d44e66f
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-08 21:09:47 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.kt
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.kt
@@ -1,3 +1,14 @@
 class Solution {
-    fun solution(hp: Int) = hp / 5 + hp % 5 / 3 + hp % 5 % 3
+    fun solution(hp: Int): Int {
+        var answer = 0
+        var remainHp = hp
+
+        answer += remainHp / 5
+        remainHp %= 5
+        answer += remainHp / 3
+        remainHp %= 3
+        answer += remainHp
+
+        return answer
+    }
 }
