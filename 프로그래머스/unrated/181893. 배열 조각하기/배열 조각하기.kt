e6e2681b13a81f81cfe1f90568966bc9611174ac
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-09 00:03:41 +0900

    [unrated] Title: 배열 조각하기, Time: 16.87 ms, Memory: 64.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun solution(arr: IntArray, query: IntArray): IntArray {
+        var answer: IntArray = arr
+
+        for(i in query.indices) {
+            if(i % 2 == 0) {
+                answer = answer.sliceArray(0..query[i])
+            } else {
+                answer = answer.sliceArray(query[i] until answer.size)
+            }
+
+        }
+
+        return answer
+    }
+}
