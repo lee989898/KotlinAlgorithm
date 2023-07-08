7ca4fc65b524d1c4d11fcdf5f6d35070b44eddac
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-08 22:56:20 +0900

    [level 0] Title: k의 개수, Time: 9.00 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.kt
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.kt
@@ -1,3 +1,11 @@
 class Solution {
-    fun solution(i: Int, j: Int, k: Int) = (i..j).joinToString("").count { it.digitToInt() == k}
+    fun solution(i: Int, j: Int, k: Int): Int {
+        var answer: Int = 0
+
+        for(num in i..j) {
+            answer += num.toString().count { it.digitToInt() == k }
+        }
+
+        return answer
+    }
 }
