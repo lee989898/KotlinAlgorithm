0e6b93fa514a1bbb218f2247c964ac3e62741d35
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-09 22:07:33 +0900

    [level 0] Title: 가까운 수, Time: 0.02 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.kt
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.kt
@@ -2,14 +2,21 @@ import kotlin.math.abs
 
 class Solution {
     fun solution(array: IntArray, n: Int): Int {
-        var answer: Int = abs(n - array[0])
-        var result = 0
+        var answer: Int = abs(array[0] - n)
+        var i = 101
 
-        array.sortedDescending().forEach { if (abs(n - it) <= answer) {
-            answer = abs(n - it)
-            result = it
-        } }
+        for (index in array.indices) {
+            if (answer > abs(array[index] - n)) {
+                    answer = abs(array[index] - n)
+                    i = array[index]
+            } else if(answer == abs(array[index] - n)) {
+                if(i > array[index]) {
+                    answer = abs(array[index] - n)
+                    i = array[index]
+                }
+            }
+        }
 
-        return result
+        return i
     }
 }
