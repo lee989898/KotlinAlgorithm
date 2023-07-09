8ab2276903a3ba3c2777f023f37c4601281600b1
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-09 16:26:08 +0900

    [unrated] Title: 문자열이 몇 번 등장하는지 세기, Time: 11.46 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.kt
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.kt
@@ -2,9 +2,9 @@ class Solution {
     fun solution(myString: String, pat: String): Int {
         var answer: Int = 0
 
-        for (i in 0..myString.length - pat.length) {
-            if (myString.slice(i until i + pat.length) == pat) {
-                answer++
+        for(i in 0..myString.length - pat.length) {
+            if(myString.slice(i until i + pat.length) == pat) {
+                answer += 1
             }
         }
 
