1d2d4549b54c94c18212dbc5d1159a70660f7ea8
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-09 14:35:42 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.kt
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.kt
@@ -1,9 +1,11 @@
 import kotlin.math.abs
 
 class Solution {
-    fun solution(a: Int, b: Int) = when {
-        a % 2 != 0 && b % 2 != 0 -> a * a + b * b
-        a % 2 == 0 && b % 2 == 0 -> abs(a - b)
-        else -> 2 * (a + b)
+    fun solution(a: Int, b: Int) = if (a % 2 != 0 && b % 2 != 0) {
+        a * a + b * b
+    } else if (a % 2 == 0 && b % 2 == 0) {
+        abs(a - b)
+    } else {
+        2 * (a + b)
     }
 }
