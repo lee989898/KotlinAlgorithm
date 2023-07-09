4154f565194b3e9ab616148a2872c0a1dfce68cc
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-09 13:07:43 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 14.92 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.kt
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.kt
@@ -1,3 +1,27 @@
+import java.util.Stack
+
 class Solution {
-    fun solution(my_string: String) = my_string.split(" ").filter { it.isNotBlank() }
+    fun solution(my_string: String): Array<String> {
+        val stack = Stack<String>()
+        var string = ""
+
+        var i = 0
+        while (i < my_string.length) {
+            if (my_string[i] == ' ') {
+                i += 1
+            } else {
+                while (i < my_string.length) {
+                    if (my_string[i] == ' ')
+                        break
+                    string += my_string[i]
+                    i += 1
+                }
+                stack.push(string)
+                string = ""
+            }
+
+        }
+
+        return stack.toTypedArray()
+    }
 }
