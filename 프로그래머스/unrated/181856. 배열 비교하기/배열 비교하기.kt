8b979f493f599b981daa58ea2fcff19904c4ab68
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-09 15:03:54 +0900

    [unrated] Title: 배열 비교하기, Time: 0.01 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.kt
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.kt
@@ -1,9 +1,14 @@
 class Solution {
-    fun solution(arr1: IntArray, arr2: IntArray) = when {
-        arr1.size > arr2.size -> 1
-        arr1.size < arr2.size -> -1
-        arr1.sum() > arr2.sum() -> 1
-        arr1.sum() < arr2.sum() -> -1
-        else -> 0
+    fun solution(arr1: IntArray, arr2: IntArray) = if (arr1.size < arr2.size) {
+        -1
+    } else if (arr1.size > arr2.size) {
+        1
+    } else {
+        if (arr1.sum() > arr2.sum())
+            1
+        else if (arr1.sum() < arr2.sum())
+            -1
+        else
+            0
     }
 }
