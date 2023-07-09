2848f8e9273099d21ba3d22528b8f88c4e89ae0c
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-09 14:56:41 +0900

    [unrated] Title: 꼬리 문자열, Time: 18.06 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.kt
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.kt
@@ -1,3 +1,3 @@
 class Solution {
-    fun solution(str_list: Array<String>, ex: String) = str_list.filter { !it.contains(ex) }.joinToString("")
+    fun solution(str_list: Array<String>, ex: String) = str_list.filterNot { it.contains(ex) }.joinToString("")
 }
