edc9309f7d14cc4772ea17133e7290c40a463be2
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-09 19:01:51 +0900

    [unrated] Title: 문자열 묶기, Time: 20.19 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.kt
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.kt
@@ -1,3 +1,7 @@
 class Solution {
-    fun solution(strArr: Array<String>) = strArr.groupBy { it.length }.maxOf { it.value.size }
+    fun solution(strArr: Array<String>): Int {
+        var answer: Int = 0
+        strArr.groupBy { it.length }.map { it.value }.forEach { if (answer <= it.size) answer = it.size }
+        return answer
+    }
 }
