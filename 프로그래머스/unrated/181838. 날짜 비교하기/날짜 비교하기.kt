fcd149bb3c9a7dfdc5bbfecb535df3a1edc31b54
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-09 16:11:30 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.02 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.kt
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.kt
@@ -1,4 +1,19 @@
 class Solution {
-    fun solution(date1: IntArray, date2: IntArray) =
-        if (date1[0] * 365 + date1[1] * 30 + date1[2] < date2[0] * 365 + date2[1] * 30 + date2[2]) 1 else 0
+    fun solution(date1: IntArray, date2: IntArray) = if (date1[0] < date2[0]) {
+        1
+    } else if (date1[0] > date2[0]) {
+        0
+    } else {
+        if (date1[1] < date2[1]) {
+            1
+        } else if (date1[1] > date2[1]) {
+            0
+        } else {
+            if (date1[2] < date2[2]) {
+                1
+            } else {
+                0
+            }
+        }
+    }
 }
