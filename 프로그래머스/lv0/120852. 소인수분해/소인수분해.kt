a9f464aed9ec70f2c39fe2c4455d6e0a6900c8f7
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-10 23:31:04 +0900

    [level 0] Title: 소인수분해, Time: 8.12 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun solution(n: Int): Set<Int> {
+        val answer = mutableSetOf<Int>()
+        var num = n
+        var i = 2
+
+        while (num > 1) {
+            if (num % i == 0) {
+                answer += i
+                num /= i
+            } else {
+                i++
+            }
+        }
+
+        return answer.toSet()
+    }
+}
