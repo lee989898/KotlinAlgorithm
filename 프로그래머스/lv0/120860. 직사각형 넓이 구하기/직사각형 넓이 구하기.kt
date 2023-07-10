ebf1062db8cbb53050ec86c8ca720db0bf1226ac
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-10 22:29:29 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 8.34 ms, Memory: 60.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.kt
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.kt
@@ -1,28 +1,8 @@
 class Solution {
     fun solution(dots: Array<IntArray>): Int {
-        var maxX = dots[0][0]
-        var maxY = dots[0][1]
-        var minX = dots[0][0]
-        var minY = dots[0][1]
-        
-        for (i in dots.indices) {
-            if(maxX < dots[i][0]) {
-                maxX = dots[i][0]
-            }
-            if (minX > dots[i][0]) {
-                minX = dots[i][0]
-            }
-        }
-        
-        for (i in  dots.indices) {
-            if(maxY < dots[i][1]) {
-                maxY = dots[i][1]
-            }
-            if (minY > dots[i][1]) {
-                minY = dots[i][1]
-            }
-        }
+        val coordX = dots.map { it[0] }.toSortedSet()
+        val coordY = dots.map { it[1] }.toSortedSet()
 
-        return (maxX - minX) * (maxY - minY)
+        return (coordX.last() - coordX.first()) * (coordY.last() - coordY.first())
     }
 }
