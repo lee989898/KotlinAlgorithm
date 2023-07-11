694af4a125f364caab69363e0a57f0cc0db8fa9c
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-11 16:12:04 +0900

    [D1] Title: 연월일 달력, Time: 123 ms, Memory: 20,452 KB -BaekjoonHub

--- SWEA/D1/2056. 연월일 달력/연월일 달력.java
+++ SWEA/D1/2056. 연월일 달력/연월일 달력.java
@@ -4,6 +4,7 @@ class Solution {
     public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
         int tc = sc.nextInt();
+        int[] days = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
         for (int i = 1; i <= tc; i++) {
             String str = sc.next();
@@ -15,26 +16,10 @@ class Solution {
             int m = Integer.parseInt(month);
             int d = Integer.parseInt(day);
 
-            if (m == 4 || m == 6 || m == 9 || m == 11) {
-                if (1 <= d && d <= 30) {
-                    str = year + "/" + month + "/" + day;
-                } else {
-                    str = "-1";
-                }
-            } else if (m == 2) {
-                if (1 <= d && d <= 28) {
-                    str = year + "/" + month + "/" + day;
-                } else {
-                    str = "-1";
-                }
-            } else if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12){
-                if (1 <= d && d <= 31) {
-                    str = year + "/" + month + "/" + day;
-                } else {
-                    str = "-1";
-                }
-            } else {
+            if (m > 12 || m == 0 || d > days[m] || d == 0) {
                 str = "-1";
+            } else {
+                str = year + "/" + month + "/" + day;
             }
 
             System.out.println("#" + i + " " + str);
