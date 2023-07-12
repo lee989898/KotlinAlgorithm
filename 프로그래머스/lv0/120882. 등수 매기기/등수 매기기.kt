4d5707f488f13c9befdd1a33fb348414cf8f3877
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-12 20:13:55 +0900

    [level 0] Title: 등수 매기기, Time: 22.16 ms, Memory: 64.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.kt
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.kt
@@ -1,6 +1,6 @@
 class Solution {
-    fun solution(score: Array<IntArray>): List<Int> {
-        val sortedScore = score.map{ it.average() }
-        return score.map { sortedScore.sortedDescending().indexOf((it[0] + it[1]) / 2.0) + 1 }
+    fun solution(score: Array<IntArray>): IntArray {
+        return score.map(IntArray::average).map { score.map(IntArray::average).sortedDescending().indexOf(it) + 1 }
+            .toIntArray()
     }
 }
