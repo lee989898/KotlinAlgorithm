e6af89bb8216a4290a73a1fd64fb9a1dbcc9ac44
lee989898 <90037701+lee989898@users.noreply.github.com>
2023-07-12 21:13:11 +0900

    [level 0] Title: 문자열 밀기, Time: 9.13 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/README.md
+++ 프로그래머스/lv0/120921. 문자열 밀기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 밀기 - 120921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120921?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120921) 
 
 ### 성능 요약
 
-메모리: 62.2 MB, 시간: 10.98 ms
+메모리: 61.7 MB, 시간: 9.13 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.kt
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.kt
@@ -1,16 +1,3 @@
 class Solution {
-    fun solution(A: String, B: String): Int {
-        var answer = 0
-        var a = A
-
-        repeat(A.length) {
-            if (a == B) {
-                return answer
-            }
-            answer+= 1
-            a = a[a.lastIndex] + a.slice(0 until a.lastIndex)
-        }
-
-        return -1
-    }
+    fun solution(A: String, B: String): Int = (B + B).indexOf(A)
 }
